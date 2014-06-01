81eeccf8c2204cabf11fd61e7634f955c2ac4266
Samuel Painter <sam.foy.painter@gmail.com>
2014-05-31 22:27:17 -0400

    insert to tree added

--- Tree.hs
+++ Tree.hs
@@ -1,2 +1,9 @@
-data (Ord a, Eq a) => Tree a = Nil | Node (Tree a) a (Tree a)
-     deriving Show
+data Tree a = Nil | Node (Tree a) a (Tree a)
+     deriving (Show, Eq)
+
+insert :: (Ord a) => Tree a -> a -> Tree a
+insert Nil x = Node (Nil) x (Nil)
+insert (Node left v right) x
+       | x == v = Node left v right
+       | x < v = Node (insert left x) v right
+       | x > v = Node left v (insert right x)
