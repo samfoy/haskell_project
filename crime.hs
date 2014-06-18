c5249a6fac699cc08f54c2238fcc0e3f9cfdba98
Samuel Painter <sam.foy.painter@gmail.com>
2014-06-18 14:09:32 -0400

    more indexing

--- crime.hs
+++ crime.hs
@@ -44,15 +44,15 @@ shuffle :: [a] -> IO [a]
 shuffle xs = do
   ar <- newArray n xs
   forM [1..n] $ \i -> do
-       j <- randomRIO (i,n)
-       vi <- readArray ar i
-       vj <- readArray ar j
-       writeArray ar j vi
-       return vj
-      where
-          n = length xs
-          newArray :: Int -> [a] -> IO (IOArray Int a)
-          newArray n = newListArray (1,n)
+   j <- randomRIO (i,n)
+   vi <- readArray ar i
+   vj <- readArray ar j
+   writeArray ar j vi
+   return vj
+  where
+      n = length xs
+      newArray :: Int -> [a] -> IO (IOArray Int a)
+      newArray n = newListArray (1,n)
 
 checkGuess :: [String] -> [String] -> Bool
 checkGuess cs gs = sort (map (map toLower) cs) == sort (map (map toLower) gs)
